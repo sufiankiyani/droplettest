b5ebc27be697f62a5a5062e4d41114d8d857092b
sufiankiyani <49144578+sufiankiyani@users.noreply.github.com>
2019-05-07 12:22:21 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'theme_dev' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'mac&123' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
